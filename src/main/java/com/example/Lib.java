d94ec4c2823e65bfe883546f0f2ddf085e655158
Fola Ayinde <kamfbd@gmail.com>
2024-04-23 17:35:27 -0500

    Initial commit

---
+++ src/main/java/com/example/Lib.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public final class Lib {
+    
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+    
+    public Integer sub(Integer a, Integer b) {
+        return a - b;
+    }
+    
+    public Integer mul(Integer a, Integer b) {
+        return a * b;
+    }
+    
+    public Integer div(Integer a, Integer b) {
+        return a / b;
+    }
+    
+}
